963b3e84facfee39384b082b01e79fea0eb72b80
Suane Vallim <suane.vallim@ilegra.com>
2021-02-28 22:41:20 -0300

    Improving the usability of the application

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengeContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile () {
+    const { level } = useContext(ChallengeContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://github.com/suanev.png" alt="Suane Vallim"/>
@@ -9,7 +13,7 @@ export function Profile () {
                 <strong>Suane Vallim</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1</p>
+                    Level {level}</p>
             </div>
         </div>
     )
